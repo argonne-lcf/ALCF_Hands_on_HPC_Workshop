2c6a173a7d9e7a54244398df360e1f660e37e911
Sam Foreman <saforem2@gmail.com>
2020-12-06 04:59:36 +0000

    Updates `hps/load_data.py` to use MNIST

--- 03_distributedHyperOpt/hps/load_data.py
+++ 03_distributedHyperOpt/hps/load_data.py
@@ -1,19 +1,36 @@
 import os
-from keras.datasets import mnist
+import numpy as np
+import tensorflow as tf
 
-def load_data():
-    """Loads the MNIST dataset.
-    Returns Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+def load_data(split: int=10000):
+    """Generate a random distribution of data for polynome_2 function: -SUM(X**2) where "**" is an element wise operator in the continuous range [a, b].
+
+    Args:
+        dim (int): size of input vector for the polynome_2 function.
+        a (int): minimum bound for all X dimensions.
+        b (int): maximum bound for all X dimensions.
+        prop (float): a value between [0., 1.] indicating how to split data between training set and validation set. `prop` corresponds to the ratio of data in training set. `1.-prop` corresponds to the amount of data in validation set.
+        size (int): amount of data to generate. It is equal to `len(training_data)+len(validation_data).
+
+    Returns:
+        tuple(tuple(ndarray, ndarray), tuple(ndarray, ndarray)): of Numpy arrays: `(train_X, train_y), (valid_X, valid_y)`.
     """
-    here = os.path.dirname(os.path.abspath(__file__))
-    directory = os.path.join(here, "DATA")
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    x_train = np.reshape(x_train, (-1, 784))
+    x_test = np.reshape(x_test, (-1, 784))
 
-    dest = os.path.join(here, 'DATA', 'mnist.npz')
+    x_val = x_train[-split:]
+    y_val = y_train[-split:]
+    x_train = x_train[:-split]
+    y_train = y_train[:-split]
 
-    (x_train, y_train), (x_test, y_test) = mnist.load_data(dest)
-    return (x_train, y_train), (x_test, y_test)
+    print(f'x_train shape: {np.shape(x_train)}')
+    print(f'y_train shape: {np.shape(y_train)}')
+    print(f'x_val shape: {np.shape(x_val)}')
+    print(f'y_val shape: {np.shape(y_val)}')
+
+    return ((x_train, y_train), (x_val, y_val))
+
 
 if __name__ == '__main__':
     load_data()
