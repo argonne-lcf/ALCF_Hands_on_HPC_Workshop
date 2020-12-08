a1683512d90b59c983d724047a82e5d78635f0e3
Taylor Childers <taylor@jtchilders.com>
2020-12-08 12:52:34 -0600

    updated submit script

--- 02_dataPipelines/00_tensorflowDatasetAPI/submit_thetagpu.sh
+++ 02_dataPipelines/00_tensorflowDatasetAPI/submit_thetagpu.sh
@@ -5,9 +5,11 @@
 #COBALT -n 1
 #COBALT -O logdir/$COBALT_JOBID
 
+echo [$SECONDS] setup conda environment
 MCONDA=/lus/theta-fs0/software/thetagpu/conda/tf_master/2020-11-11/mconda3
 source $MCONDA/setup.sh
 
+echo [$SECONDS] setup local env vars
 NODES=`cat $COBALT_NODEFILE | wc -l`
 PPN=1
 RANKS=$((NODES * PPN))
@@ -15,5 +17,6 @@ echo NODES=$NODES  PPN=$PPN  RANKS=$RANKS
 
 export OMP_NUM_THREADS=64
 
+echo [$SECONDS] run example
 python ilsvrc_dataset.py -c ilsvrc.json --interop $OMP_NUM_THREADS --intraop $OMP_NUM_THREADS \
    --logdir logdir/$COBALT_JOBID $HOROVOD 
