9c22829b4eb886fcd967eba11f5d8304a582b62e
Huihuo Zheng <huihuo.zheng@anl.gov>
2020-12-08 14:59:28 -0600

    Update qsub_tensorflow_cifar10.sh

--- 01_distributedDeepLearning/Horovod/submissions/thetagpu/qsub_tensorflow_cifar10.sh
+++ 01_distributedDeepLearning/Horovod/submissions/thetagpu/qsub_tensorflow_cifar10.sh
@@ -10,7 +10,8 @@ echo "Running Cobalt Job $COBALT_JOBID."
 #Loading modules
 
 #source /lus/theta-fs0/software/datascience/thetagpu/anaconda3/setup.sh
-source /lus/theta-fs0/software/thetagpu/conda/tf_master/latest/mconda3/setup.sh
+source /lus/theta-fs0/software/thetagpu/conda/tf_master/2020-11-11/mconda3/setup.sh
+#source /lus/theta-fs0/software/thetagpu/conda/tf_master/latest/mconda3/setup.sh
 
 COBALT_JOBSIZE=$(cat $COBALT_NODEFILE | wc -l)
 
