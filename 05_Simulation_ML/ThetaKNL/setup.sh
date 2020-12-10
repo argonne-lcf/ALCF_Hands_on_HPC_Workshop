0e241e7f9641fd72be4a9358166f3ba85cb43fb7
Bethany Lusch <bethanyl@anl.gov>
2020-12-09 22:40:29 -0600

    remove unnecessary libraries

--- 05_Simulation_ML/ThetaKNL/setup.sh
+++ 05_Simulation_ML/ThetaKNL/setup.sh
@@ -2,14 +2,12 @@
 unset PYTHONPATH
 export CRAYPE_LINK_TYPE=dynamic
 
-module use /soft/datascience/a21/modulefiles
 module load intelpython36
 
 TF_DIR=/soft/datascience/tensorflow/tf2.2-py36
 TORCH_DIR=/soft/datascience/pytorch/1.4.0-py36/lib/python3.6/site-packages
 
 export PYTHONPATH=${TF_DIR}:${TORCH_DIR}:$PYTHONPATH
-export LD_LIBRARY_PATH=/soft/datascience/a21/oneccl/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/cray/pe/mpt/7.7.14/gni/mpich-intel-abi/16.0/lib:$LD_LIBRARY_PATH
 
 # Clean up the directory
