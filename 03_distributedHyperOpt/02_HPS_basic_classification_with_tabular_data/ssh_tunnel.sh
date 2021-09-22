7d34fd58e73a3428a024f3782c5bd79ee5d3ff23
Deathn0t <romainegele@gmail.com>
2021-09-22 19:22:27 +0200

    update ssh tunnel script

--- 03_distributedHyperOpt/02_HPS_basic_classification_with_tabular_data/ssh_tunnel.sh
+++ 03_distributedHyperOpt/02_HPS_basic_classification_with_tabular_data/ssh_tunnel.sh
@@ -8,5 +8,5 @@ export THETAGPU_NODE=thetagpu05
 # port on your local machine
 # picking a number other than the default 8888 is recommended
 export PORT_NUM=8889
-ssh -L $PORT_NUM:localhost:8888 $ALCF_USERNAME@theta.alcf.anl.gov \
+ssh -tt -L $PORT_NUM:localhost:8888 $ALCF_USERNAME@theta.alcf.anl.gov \
     "ssh -L 8888:localhost:$JUPYTER_PORT" $THETAGPU_NODE
