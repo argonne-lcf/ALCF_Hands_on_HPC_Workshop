1bb31d61aeec59e182712aa2b8a4a4d0ed587c71
Deathn0t <romainegele@gmail.com>
2021-09-30 08:04:25 -0500

    edit launch_ray_cluster

--- 03_distributedHyperOpt/02_NAS_and_more/launch-ray-cluster.sh
+++ 03_distributedHyperOpt/02_NAS_and_more/launch-ray-cluster.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
 
-# User Configuration
-EXP_DIR=$PWD
-INIT_SCRIPT=$PWD/init-dh-environment.sh
-CPUS_PER_NODE=8
+# USER CONFIGURATION
+CURRENT_DIR=$PWD
+CPUS_PER_NODE=64
 GPUS_PER_NODE=8
 
-# Initialization of environment
-source $INIT_SCRIPT
+# Script to launch Ray cluster
 
-# Collect IP addresses of available compute nodes
+ACTIVATE_PYTHON_ENV=$PWD/init-dh-environment.sh
+echo "Script to activate Python env: $ACTIVATE_PYTHON_ENV"
+
+
+# Getting the node names
 mapfile -t nodes_array -d '\n' < $COBALT_NODEFILE
-HEAD_NODE=${nodes_array[0]}
-HEAD_NODE_IP=$(dig $HEAD_NODE a +short | awk 'FNR==2')
-echo "Detected HEAD node $HEAD_NODE with IP $HEAD_NODE_IP"
 
-WORKER_NODES=${nodes_array[@]:1}
-for ((i=0; i < ${#WORKER_NODES[@]}; i++)); do
-    WORKER_NODES_IPS[$i]=$(dig ${WORKER_NODES[$i]} a +short | awk 'FNR==1')
-done
-echo "Detected ${#WORKER_NODES[@]} workers with IPs: ${WORKER_NODES_IPS[@]}"
+head_node=${nodes_array[0]}
+head_node_ip=$(dig $head_node a +short | awk 'FNR==2')
 
-# Launch the Ray cluster
+# if we detect a space character in the head node IP, we'll
+# convert it to an ipv4 address. This step is optional.
+if [[ "$head_node_ip" == *" "* ]]; then
+IFS=' ' read -ra ADDR <<<"$head_node_ip"
+if [[ ${#ADDR[0]} -gt 16 ]]; then
+head_node_ip=${ADDR[1]}
+else
+head_node_ip=${ADDR[0]}
+fi
+echo "IPV6 address detected. We split the IPV4 address as $head_node_ip"
+fi
+
 # Starting the Ray Head Node
-RAY_PORT=6379
+port=6379
+ip_head=$head_node_ip:$port
+export ip_head
+echo "IP Head: $ip_head"
 
-echo "Starting HEAD at $HEAD_NODE_IP"
-ssh -tt $HEAD_NODE_IP "source $INIT_SCRIPT; \
-    ray start --head --node-ip-address=$HEAD_NODE_IP --port=$RAY_PORT \
-    --num-cpus $CPUS_PER_NODE --num-gpus $GPUS_PER_NODE" &
+echo "Starting HEAD at $head_node"
+ssh -tt $head_node_ip "source $ACTIVATE_PYTHON_ENV; cd $CURRENT_DIR; \
+    ray start --head --node-ip-address=$head_node_ip --port=$port \
+    --num-cpus ${CPUS_PER_NODE-1} --num-gpus ${GPUS_PER_NODE-1} --block" &
 
 # optional, though may be useful in certain versions of Ray < 1.0.
 sleep 10
 
 # number of nodes other than the head node
-for ((i=0; i < ${#WORKER_NODES_IPS[@]}; i++)); do
-    echo "Starting WORKER $i at ${WORKER_NODES_IPS[$i]}"
-    ssh -tt ${WORKER_NODES_IPS[$i]} "source $INIT_SCRIPT && \
-        ray start --address $HEAD_NODE_IP:$RAY_PORT \
-        --num-cpus $CPUS_PER_NODE --num-gpus $GPUS_PER_NODE" &
+worker_num=$((${#nodes_array[*]} - 1))
+echo "$worker_num workers"
+
+for ((i = 1; i <= worker_num; i++)); do
+    node_i=${nodes_array[$i]}
+    node_i_ip=$(dig $node_i a +short | awk 'FNR==1')
+    echo "Starting WORKER $i at $node_i with ip=$node_i_ip"
+    ssh -tt $node_i_ip "source $ACTIVATE_PYTHON_ENV; cd $CURRENT_DIR; \
+        ray start --address $ip_head \
+        --num-cpus $CPUS_PER_NODE --num-gpus $GPUS_PER_NODE --block" &
     sleep 5
 done
