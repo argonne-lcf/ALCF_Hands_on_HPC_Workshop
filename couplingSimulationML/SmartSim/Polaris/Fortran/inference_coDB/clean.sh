f73457c175bdafce4ce0c57c07e0fc7edb50c899
rickybalin <riccardo.balin@colorado.edu>
2022-10-02 14:50:24 -0400

    Update to coupling Sim and AI with SmartSim (#16)
    * Some changes to introductory material
    
    * Cleaned up SmartSim folder from SDL_2021 and added new discussion of this approach
    
    * Testing adding caption to figure
    
    * Added captions to figures
    
    * Cleaned up more of last year's material
    
    * Added description of training and inference examples
    
    * Fixed typos
    
    * Trying to get math symbols in bullets
    
    * Added Fortran build files for training examples and updated README with build instructions
    
    * Fixed some typos
    
    * Minor update
    
    * Added temporary installation script and fixed typos and links
    
    * Typos
    
    * Added Python examples. All work with temporary env for SDL.
    
    * Updated Fortran data loader but needs to be tested
    
    * Fortran train_coDB example works
    
    * Added rest of Fortran examples. All work.
    
    * Small changes

---
+++ couplingSimulationML/SmartSim/Polaris/Fortran/inference_coDB/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm inf_coDB.*
+rm input.config
+rm inference-db.log
+rm inference.out inference.err
+rm predictions_node*
+rm .colocated_launcher.sh
---
+++ couplingSimulationML/SmartSim/Polaris/Python/inference_coDB/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm inf_coDB.*
+rm inference-db.log
+rm inference.out inference.err
+rm predictions_node*
+rm .colocated_launcher.sh
