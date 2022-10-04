63e0c6551ac3e5ad817226e8c51b6bc592289dfb
Filippo Simini <filippo.simini@gmail.com>
2022-10-04 02:04:14 -0500

    added PyTorchProfiler

---
+++ profilingDeepLearning/PyTorchProfiler/example1/v3.py
@@ -0,0 +1,14 @@
+# This example is adapted from
+# https://pytorch.org/tutorials/recipes/recipes/profiler_recipe.html
+
+import torch
+import torchvision.models as models
+import torch.autograd.profiler as profiler
+
+torch.set_default_tensor_type(torch.FloatTensor)
+model = models.resnet18()
+inputs = torch.randn(128, 3, 224, 224)
+
+with profiler.profile() as prof:
+    model(inputs)
+print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
---
+++ profilingDeepLearning/PyTorchProfiler/example2/v3.py
@@ -0,0 +1,41 @@
+# This example is adapted from
+# https://pytorch.org/tutorials/beginner/profiler.html
+
+import torch
+import numpy as np
+
+
+class MyModule(torch.nn.Module):
+    def __init__(
+            self, in_features: int,
+            out_features: int,
+            hidden_sizes: list,
+            bias: bool = True):
+        super(MyModule, self).__init__()
+
+        sizes = [in_features] + hidden_sizes + [out_features]
+        layers = []
+        for s in range(len(sizes)-1):
+            layers.append(torch.nn.Linear(sizes[s], sizes[s+1], bias))
+            layers.append(torch.nn.ReLU())
+        self.linear = torch.nn.Sequential(*layers)
+
+    def forward(self, input, mask):
+        out = self.linear(input)
+
+        threshold = out.sum(axis=1).mean().item()
+        hi_idx = np.argwhere(mask.cpu().numpy() > threshold)
+        hi_idx = torch.from_numpy(hi_idx).cuda()
+
+        return out, hi_idx
+
+
+torch.set_default_tensor_type(torch.cuda.FloatTensor)
+model = MyModule(512, 8, [32, 32, 32])
+input = torch.rand(512 * 512, 512)
+mask = torch.rand((512, 512, 512))
+
+# warm-up
+model(input, mask)
+
+out, idx = model(input, mask)
