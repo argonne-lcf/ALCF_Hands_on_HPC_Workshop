4e46e4fa58a5459af062b036a27e66ad096a7f12
balin <riccardo.balin@colorado.edu>
2023-10-12 21:15:21 +0000

    Updated SmartSim+NekRS examples to start from checkpoint file

--- couplingSimulationML/NekRS-ML/inference_example/run.sh
+++ couplingSimulationML/NekRS-ML/inference_example/run.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-python ssim_driver_polaris.py sim.executable=$NEKRS_HOME/bin/nekrs run_args.simprocs=3 run_args.simprocs_pn=3 inference.model_path=./model_jit.pt inference.device=GPU:3
+# Set run env
+nodes=`wc -l < $PBS_NODEFILE`
+striping_unit=16777216
+max_striping_factor=128
+let striping_factor=$nodes/2
+if [ $striping_factor -gt $max_striping_factor ]; then
+  striping_factor=$max_striping_factor
+fi
+if [ $striping_factor -lt 1 ]; then
+  striping_factor=1
+fi
+MPICH_MPIIO_HINTS="*:striping_unit=${striping_unit}:striping_factor=${striping_factor}:romio_cb_write=enable:romio_ds_write=disable:romio_no_indep_rw=true"
+
+ulimit -s unlimited
+export NEKRS_GPU_MPI=1
+export MPICH_MPIIO_HINTS=$MPICH_MPIIO_HINTS
+export MPICH_MPIIO_STATS=1
+export NEKRS_CACHE_BCAST=1
+export NEKRS_LOCAL_TMP_DIR=/local/scratch
+export MPICH_GPU_SUPPORT_ENABLED=1
+#export MPICH_OFI_NIC_POLICY=NUMA
+export FI_OFI_RXM_RX_SIZE=32768
+
+# Run the driver script
+sim_arguments="--setup turbChannel.par --backend CUDA"
+python ssim_driver_polaris.py \
+  sim.executable=$NEKRS_HOME/bin/nekrs run_args.simprocs=3 run_args.simprocs_pn=3 \
+  sim.arguments="${sim_arguments}" \
+  inference.model_path=./model_jit.pt inference.device=GPU:3
--- couplingSimulationML/NekRS-ML/train_example/affinity_nrs.sh
+++ couplingSimulationML/NekRS-ML/train_example/affinity_nrs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 num_gpus=$1
-offset=$2
 shift
-shift
-gpu=$((${PMI_LOCAL_RANK} % ${num_gpus} + ${offset} ))
+gpu=$((${PMI_LOCAL_RANK} % ${num_gpus} ))
 export CUDA_VISIBLE_DEVICES=$gpu
 echo ?RANK= ${PMI_RANK} LOCAL_RANK= ${PMI_LOCAL_RANK} gpu= ${gpu}?
 exec "$@"
