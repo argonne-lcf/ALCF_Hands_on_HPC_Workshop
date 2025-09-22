1403d07af6cc83f9319af452e80b12b2c291032f
Riccardo Balin <riccardo.balin@gmail.com>
2025-09-22 20:45:53 +0000

    Remove old DPEP material from AI_frameworks

--- data_science/AI_frameworks/DPEP/examples/dpnp_matmul.py
+++
@@ -1,9 +0,0 @@
-import dpnp as np
-from time import perf_counter
-
-x = np.random.randn(1000,1000)
-tic = perf_counter()
-y = np.matmul(x,x)
-y.sycl_queue.wait()
-print(f"Execution time: {perf_counter() - tic} sec")
-
--- data_science/AI_frameworks/DPEP/examples/dpnp_sum.py
+++
@@ -1,8 +0,0 @@
-import dpnp as np 
-
-x = np.asarray([1, 2, 3])
-print("Array x allocated on the device:", x.device)
-
-y = np.sum(x)
-print("Result y is located on the device:", y.device)
-
