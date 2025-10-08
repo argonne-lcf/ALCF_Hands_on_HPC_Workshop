1672fcedf8555d4c4a7eb3d6933b4b1b5ac3fc8d
Khalid Hossain <khossain4337@gmail.com>
2025-10-08 06:03:06 +0000

    Made a last check run. Added directory for saving traces, and example traces

--- data_science/ai_ml_profiling/run_ddp_pytorch_2p8_compile_N1_R12.sh
+++ data_science/ai_ml_profiling/run_ddp_pytorch_2p8_compile_N1_R12.sh
@@ -1,24 +1,6 @@
 #! /bin/bash -x
 #
-## Load modules 2025.1.3 with runtime (UMD) 1099.17
-#
-#module restore
-#module unload mpich oneapi
-#module use /soft/compilers/oneapi/nope/modulefiles
-#module use /soft/compilers/oneapi/2025.1.3/modulefiles
-#module use /soft/preview/components/graphics-compute-runtime/1099.17/modulefiles
-#module add mpich/nope/develop-git.6037a7a
-#module add oneapi/public/2025.1.3
-#module add graphics-compute-runtime/1099.17
-
-#module load cmake
-#unset CMAKE_ROOT
-
-#source /opt/aurora/24.347.0/spack/unified/0.9.2/install/linux-sles15-x86_64/gcc-13.3.0/miniforge3-24.3.0-0-gfganax/bin/activate
-#conda activate /lus/flare/projects/datasets/softwares/envs/vLLM_main_pytorch_2p8_oneapi_2025p1p3_pti_0p10p3_python3p10_julia
-#source /lus/flare/projects/datasets/softwares/envs/pytorch_2p8_oneapi_2025p1p3_umd_1099p17_mpi4py/bin/activate
-
-source /lus/flare/projects/datasets/softwares/training/hpc_hands_10_07_2025/for_workshop/hpc_hands_on_2025_pytorch_2p8.env
+source ./hpc_hands_on_2025_pytorch_2p8.env
 
 export ZE_FLAT_DEVICE_HIERARCHY=FLAT
 export FI_MR_CACHE_MONITOR=userfaultfd
